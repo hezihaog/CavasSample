95e78439e309e1dda918314e99574dcc52b5c210
zihe <hezihao@linghit.com>
2020-02-01 23:02:39 +0800

    增加vivo网页Web端的loading效果（gif图）。

--- app/src/main/java/com/zh/cavas/sample/MainActivity.java
+++ app/src/main/java/com/zh/cavas/sample/MainActivity.java
@@ -2,6 +2,7 @@ package com.zh.cavas.sample;
 
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,9 +11,16 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.zh.cavas.sample.widget.BackArrowView;
 import com.zh.cavas.sample.widget.DownloadProgressView;
 import com.zh.cavas.sample.widget.HookCheckBox;
@@ -27,6 +35,7 @@ public class MainActivity extends BaseActivity {
     private SeekBar vVivoSeekBarGray;
     private HookCheckBox vHookCheckBox;
     private ImageView vVivoLoadingProgress;
+    private ImageView vVivoPolygonLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +55,7 @@ public class MainActivity extends BaseActivity {
         vVivoSeekBarGray = view.findViewById(R.id.vivo_seek_bar_gray);
         vHookCheckBox = view.findViewById(R.id.hook_checkbox);
         vVivoLoadingProgress = view.findViewById(R.id.vivo_loading_progress);
+        vVivoPolygonLoading = view.findViewById(R.id.vivo_polygon_loading);
     }
 
     private void bindView() {
@@ -105,6 +115,21 @@ public class MainActivity extends BaseActivity {
         if (animationDrawable != null && !animationDrawable.isRunning()) {
             animationDrawable.start();
         }
+        Glide.with(this).load(R.drawable.vivo_polygon_loading).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target target, boolean isFirstResource) {
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target target, DataSource dataSource, boolean isFirstResource) {
+                if (resource instanceof GifDrawable) {
+                    //无限循环
+                    ((GifDrawable) resource).setLoopCount(GifDrawable.LOOP_FOREVER);
+                }
+                return false;
+            }
+        }).into(vVivoPolygonLoading);
     }
 
     @Override
